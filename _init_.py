890231dbaf5cb2caa15d3e0123b8f96a9ecc4fcc
khushjain15 <153334827+khushjain15@users.noreply.github.com>
2025-08-01 10:30:45 -0700

    Create _init_.py

---
+++ _init_.py
@@ -0,0 +1,9 @@
+"""
+Azure Resource Provisioning Agent for Teams Chatbot
+
+This package provides an intelligent agent that can provision Azure resources
+when invoked through a Microsoft Teams chatbot.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Azure Provisioning Agent Team"
